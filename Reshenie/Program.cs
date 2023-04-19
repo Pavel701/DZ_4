64e8836a80c9eaca0defd88f12db408c23e3d28a
flip701 <paulshipspb@gmail.com>
2023-04-19 16:46:48 +0300

    Решение первой задачи

---
+++ Reshenie/Program.cs
@@ -0,0 +1,17 @@
+﻿
+    Console.Write("Введите число A: ");
+    int a = int.Parse(Console.ReadLine());
+
+    Console.Write("Введите число B: ");
+    int b = int.Parse(Console.ReadLine());
+
+    int result = 1;
+
+    for (int i = 1; i <= b; i++) {
+      result *= a;
+    }
+
+    Console.WriteLine($"{a}^{b} = {result}");
+  
+
+
