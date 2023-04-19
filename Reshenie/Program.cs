abd68f29eac56ed940f719a059eca39ac42ef7d6
flip701 <paulshipspb@gmail.com>
2023-04-19 16:51:53 +0300

    Решение третей задачи

--- Reshenie/Program.cs
+++ Reshenie/Program.cs
@@ -19,17 +19,30 @@
 //452 -> 11
 
 
-    Console.Write("Введите число: ");
-    int number = int.Parse(Console.ReadLine());
+    // Console.Write("Введите число: ");
+    // int number = int.Parse(Console.ReadLine());
 
-    int sum = 0;
+    // int sum = 0;
 
-    while (number > 0) {
-      int digit = number % 10;
-      sum += digit;
-      number /= 10;
+    // while (number > 0) {
+    //   int digit = number % 10;
+    //   sum += digit;
+    //   number /= 10;
+    // }
+
+    // Console.WriteLine($"Сумма цифр числа: {sum}");
+  
+
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+    int[] numbers = new int[8] { 2, 5, 7, 1, 8, 4, 6, 3 };
+
+    Console.WriteLine("Элементы массива:");
+
+    for (int i = 0; i < numbers.Length; i++) {
+      Console.Write($"{numbers[i]} ");
     }
 
-    Console.WriteLine($"Сумма цифр числа: {sum}");
-  
+    Console.WriteLine();
+
 
