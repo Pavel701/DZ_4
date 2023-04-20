2f6a5566eeb796c96c8f8490d163b1c78eb5bf88
flip701 <paulshipspb@gmail.com>
2023-04-20 08:11:40 +0300

    В третью задачу добавлена сумма элементов массива

--- Reshenie/Program.cs
+++ Reshenie/Program.cs
@@ -1,48 +1,50 @@
 ﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 //3, 5 -> 243 (3⁵), 2, 4 -> 16
 
-    // Console.Write("Введите число A: ");
-    // int a = int.Parse(Console.ReadLine());
+    Console.Write("Введите число A: ");
+    int a = int.Parse(Console.ReadLine());
 
-    // Console.Write("Введите число B: ");
-    // int b = int.Parse(Console.ReadLine());
+    Console.Write("Введите число B: ");
+    int b = int.Parse(Console.ReadLine());
 
-    // int result = 1;
+    int result = 1;
 
-    // for (int i = 1; i <= b; i++) {
-    //   result *= a;
-    // }
+    for (int i = 1; i <= b; i++) {
+      result *= a;
+    }
 
-    // Console.WriteLine($"{a}^{b} = {result}");
+    Console.WriteLine($"{a}^{b} = {result}");
   
 //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 //452 -> 11
 
 
-    // Console.Write("Введите число: ");
-    // int number = int.Parse(Console.ReadLine());
+    Console.Write("Введите число: ");
+    int number = int.Parse(Console.ReadLine());
 
-    // int sum = 0;
+    int sum = 0;
 
-    // while (number > 0) {
-    //   int digit = number % 10;
-    //   sum += digit;
-    //   number /= 10;
-    // }
+    while (number > 0) {
+      int digit = number % 10;
+      sum += digit;
+      number /= 10;
+    }
 
-    // Console.WriteLine($"Сумма цифр числа: {sum}");
+    Console.WriteLine($"Сумма цифр числа: {sum}");
   
 
 //Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-    int[] numbers = new int[8] { 2, 5, 7, 1, 8, 4, 6, 3 };
-
-    Console.WriteLine("Элементы массива:");
-
-    for (int i = 0; i < numbers.Length; i++) {
-      Console.Write($"{numbers[i]} ");
+    int sum = 0;       
+int[] arr = new int[8] { 2, 7, 1, 5, 8, 4, 6, 3 };
+Console.WriteLine("Элементы массива:");
+for (int i = 0; i < arr.Length; i++) {
+Console.Write($"{arr[i]} \n");
     }
-
-    Console.WriteLine();
+for (int i = 0; i < arr.Length; i++)
+ {
+  sum += arr[i];
+   }
+  Console.Write($"Сумма элементов массива: {sum} ");
 
 
